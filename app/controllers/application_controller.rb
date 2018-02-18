1aa0e54b3193f19d7f193b6e5dbd80594b38110c
GutuGaluppo <gutu.galuppo@gmail.com>
2018-02-18 12:21:29 +0100

    change root route to display goodbye action
    texto grande inutil...

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  def goodbye
+    render html: "goodbyekhgajhhlijhgkvdgfylkgh dsfb vbfv yugadfxv cigjkab nv yluihfd"
+  end
+
   def hello
     render html: "halo mundoi!"
   end
